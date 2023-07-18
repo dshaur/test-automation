8014d8696a53c51598c09449eded02ff053d51bc
dshaur <shaur.david@gmail.com>
2023-07-18 13:43:41 -0400

    Fixed POST request json file to use Freemarker for the case when the title gets removed. Removed unused exceptions. Fixed Album HTTP Method classes to use config.env instead of replacing url placeholders in the constructors. Fixed PATCH rs.json to only expect the updated property. Still working on testPatchAlbum() method to use ID from POST and call it in the PATCH.

--- src/main/java/com/solvd/carina/demo/api/albums/GetAlbumMethod.java
+++ src/main/java/com/solvd/carina/demo/api/albums/GetAlbumMethod.java
@@ -6,13 +6,11 @@ import com.zebrunner.carina.api.annotation.ResponseTemplatePath;
 import com.zebrunner.carina.api.annotation.SuccessfulHttpStatus;
 import com.zebrunner.carina.api.http.HttpMethodType;
 import com.zebrunner.carina.api.http.HttpResponseStatusType;
-import com.zebrunner.carina.utils.config.Configuration;
 
 @Endpoint(url = "${config.env.base_url}/albums", methodType = HttpMethodType.GET)
 @ResponseTemplatePath(path = "api/albums/_get/rs.json")
 @SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
 public class GetAlbumMethod extends AbstractApiMethodV2 {
     public GetAlbumMethod() {
-        replaceUrlPlaceholder("base_url", Configuration.getRequired("api_url"));
     }
 }
